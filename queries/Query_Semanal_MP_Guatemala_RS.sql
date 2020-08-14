2c83d9e19291adfdede907e896ba2f23839deeed
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-08-14 10:25:36 -0600

    SQL

---
+++ queries/Query_Semanal_MP_Guatemala_RS.sql
@@ -0,0 +1,48 @@
+select a.fecha, a.total, b.total2 from (    
+	-- PROGRAMADOS
+	select if(weekofyear(fecho_detec),weekofyear(fecho_detec),weekofyear(fecho_repar))
+	as fecha, count(averia.averia_id) as total
+	from movistar_gua.averia 
+	where (tipo_rep_id=2 and proceso_id=7 AND informante_id=3 AND fecho_detec <> 0 AND 
+	((weekofyear(fecho_detec) >= weekofyear('2013-10-01') 
+	and weekofyear(fecho_detec) <= weekofyear(DATE_ADD('2013-10-01',INTERVAL  30 day)) and fecho_detec != 0) or
+	(weekofyear(fecho_repar) >= weekofyear('2013-10-01') 
+	and  weekofyear(fecho_repar) <= weekofyear(DATE_ADD('2013-10-01',INTERVAL  30 day)) and fecho_detec = 0)))
+	GROUP by fecha
+	)as a left join (                 
+	-- EJECUTADOS
+	select if(weekofyear(fecho_detec),weekofyear(fecho_detec),weekofyear(fecho_repar)) as fecha, count(averia_id) as total2 
+	from movistar_gua.averia 
+	where tipo_rep_id=2  and weekofyear(fecho_repar) >= weekofyear('2013-10-01') 
+	and  weekofyear(fecho_repar) <= weekofyear(DATE_ADD('2013-10-01',INTERVAL  30 day)) 
+	and proceso_id=7 AND informante_id=3  GROUP by fecha
+	) as b on a.fecha=b.fecha;
+
+				
+select a.fecha, a.total, b.total2 from (    
+	-- programados
+	select if( if(weekofyear(fecho_detec),weekofyear(fecho_detec),0) >= if(weekofyear(fecho_repar),weekofyear(fecho_repar),0),
+	if(weekofyear(fecho_repar), weekofyear(fecho_repar), weekofyear(fecho_detec)),weekofyear(fecho_detec)) as fecha,
+	count(averia.averia_id) as total 
+	from movistar_gua.averia 
+	where tipo_rep_id=2 and proceso_id=7 AND informante_id=3 and fecho_detec != 0 and 
+	((weekofyear(fecho_detec) >= weekofyear('2013-10-01') 
+	and weekofyear(fecho_detec) <= weekofyear(DATE_ADD('2013-10-01',INTERVAL  31 day)) and fecho_detec != 0) or
+	(weekofyear(fecho_repar) >= weekofyear('2013-10-01') 
+	and  weekofyear(fecho_repar) <= weekofyear(DATE_ADD('2013-10-01',INTERVAL  31 day)) and fecho_detec = 0) or
+	(weekofyear(fecho_repar) >= weekofyear('2013-10-01') 
+	and  weekofyear(fecho_repar) <= weekofyear(DATE_ADD('2013-10-01',INTERVAL  31 day)) 
+	and fecho_detec != 0 and fecho_repar != 0))
+	GROUP by fecha
+	)as a left join (                 
+	-- mantenimientos ejecutados
+	select if( 
+	if(weekofyear(fecho_detec),weekofyear(fecho_detec),0) >= if(weekofyear(fecho_repar),weekofyear(fecho_repar),0),
+	if(weekofyear(fecho_repar), weekofyear(fecho_repar), weekofyear(fecho_detec)),
+	if(weekofyear(fecho_detec), weekofyear(fecho_detec), weekofyear(fecho_repar))) as fecha,
+	count(averia_id) as total2 
+	from movistar_gua.averia 
+	where tipo_rep_id=2  and weekofyear(fecho_repar) >= weekofyear('2013-10-01') 
+	and  weekofyear(fecho_repar) <= weekofyear(DATE_ADD('2013-10-01',INTERVAL  30 day)) 
+	and proceso_id=7 AND informante_id=3  GROUP by fecha
+	) as b on a.fecha=b.fecha;
